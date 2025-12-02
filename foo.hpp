20c190de2037f59f896dadcd44795099ca4609bf
pprusaczyk <pawlop1112@gmail.com>
2025-12-02 23:59:33 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,7 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](Human& h){ h.birthday(); return h.isMonster() ? 'n' : 'y'; });
 
     return retval;
 }
